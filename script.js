20989b98cb99d36bc103e075be83ebc85a0859de
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-06-29 23:46:00 +0530

    color changed

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var accordian = document.getElementsByClassName("content-container");
 for (let i = 0; i < accordian.length; i++) {
     accordian[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        if (document.body.style.background === "rgb(100, 124, 99)") {
+        if (document.body.style.background === "rgb(50, 50, 100)") {
             document.body.style.transition='1s'
-            document.body.style.backgroundColor = ""; // Reset to default color
+            document.body.style.background = "black"; // Reset to default color
         } else {
             document.body.style.transition='1s'
-            document.body.style.background = "rgb(100, 124, 99)"; // Replace "red" with your desired color
+            document.body.style.background = "rgb(50, 50, 100)"; // Replace "red" with your desired color
         }
         
     });
